e6ed6237bebdc0695a028c2e2eb6f192de838251
mrpeverill <mrpeverill@gmail.com>
2022-04-01 11:20:53 -0500

    initial-commit

---
+++ commoninit.el
@@ -0,0 +1,43 @@
+
+(load "centered-window-mode/centered-window.el")
+(load "mpages/mpages.el")
+
+(autoload 'markdown-mode "markdown-mode"
+   "Major mode for editing Markdown files" t)
+(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+
+(autoload 'gfm-mode "markdown-mode"
+   "Major mode for editing GitHub Flavored Markdown files" t)
+(add-to-list 'auto-mode-alist '("README\\.md\\'" . gfm-mode))
+
+;; Save temp files in tmp
+(setq backup-directory-alist
+`((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms
+`((".*" ,temporary-file-directory t)))
+
+;; Don't show startup
+(setq inhibit-startup-screen t)
+
+;; use the recycling
+(setq delete-by-moving-to-trash t)
+
+;;system specific variables
+(custom-set-variables
+ ;; where are mpages stored?
+ (let ((default-directory system-user-path-dropbox))
+   '(mpages-content-directory (expand-file-name "mpages/"))
+   )
+ '(smtpmail-smtp-server "Smtp.gmail.com")
+ '(smtpmail-smtp-service 25)
+ )
+
+;; font and theme settings.
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes '(wombat))
+ '(package-selected-packages '(csv-mode)))
---
+++ windows-fonts.el
@@ -0,0 +1,8 @@
+;;These font settings are windows specific
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Consolas" :foundry "outline" :slant normal :weight normal :height 112 :width normal))))
+ '(fringe ((t (:background "#242424")))))
